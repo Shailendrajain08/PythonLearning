1ab130b3131d251f9d9e24202bee830229e526ad
Shailendrajain08 <shailendra.jain0894@gmail.com>
2024-04-09 16:40:41 -0400

    comment and escape sequence

---
+++ EscapeSeqAndComm.py
@@ -0,0 +1,23 @@
+# Start Code from here                       // # is used for commenting in Python
+# type sonthing 
+
+
+print("Hey My Name is Shailendra Jain\nI am a software engineer. ")  # \n is used for give new line or give a escape sequence
+
+
+# tripple single (') quote or tripple double (") are used to give multiline comment.
+'''
+Hey I am
+Shailendra Jain
+'''
+"""
+I am a 
+Software Engineer
+"""
+
+# for escaping "" we use \""\ in python 
+
+print("Hey my name is Shailendra Jain \"I am a Very Good\"\nSoftware Engineer")
+
+print("hey", 6, 7, sep="~", end="009\n")
+print("Shailendra")
