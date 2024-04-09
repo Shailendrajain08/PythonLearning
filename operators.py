3be619159ae598edd4bf6fd3aa75fe13ec974c26
Shailendrajain08 <shailendra.jain0894@gmail.com>
2024-04-09 16:41:06 -0400

    Operators and simple use of operators

---
+++ operators.py
@@ -0,0 +1,20 @@
+print(15+7)
+print(15-7)
+print(15*7)
+print(15/7)
+print(15//7)
+print(15%7)
+print(15**2)
+
+# Basic Calculator 
+# ---------------------------------------
+
+a = 56
+b = 3
+
+print("value of a ", a, " + ", 3, " is ", a+b)
+print("value of a ", a, " - ", 3, " is ", a-b)
+print("value of a ", a, " * ", 3, " is ", a*b)
+print("value of a ", a, " / ", 3, " is ", a/b)
+print("value of a ", a, " % ", 3, " is ", a%b)
+print("value of a ", a, " // ", 3, " is ", a//b)
