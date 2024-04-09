ba741510bddfdacc5775d55415c626ba57ac393a
Shailendrajain08 <shailendra.jain0894@gmail.com>
2024-04-09 16:40:50 -0400

    Data type and variable

---
+++ dataTypeVariable.py
@@ -0,0 +1,15 @@
+a = 123;
+print(a)
+a1 = 9;
+b="Harry";
+print(b)
+print(a + a1)
+c = 8.1
+d = complex(8, 2)
+e = False;
+print("Type of a is", type(a))
+print("Type of a1 is", type(a1))
+print("Type of b is", type(b))
+print("Type of c is", type(c))
+print("Type of d is", type(d))
+print("Type of e is", type(e))
