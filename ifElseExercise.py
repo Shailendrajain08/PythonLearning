9a8f6f9a6401587423c36f0ce7f4f93d0859293b
Shailendrajain08 <shailendra.jain0894@gmail.com>
2024-04-10 16:26:27 -0400

    Complete Good morning sir exersice

--- ifElseExercise.py
+++ ifElseExercise.py
@@ -1,10 +1,11 @@
 import time
-timestamp = int(time.strftime('%H'))
+# timestamp = int(time.strftime('%H'))
+timestamp =  int(input("enter hours: "))
 print(timestamp)
 
 if(timestamp >= 0 and timestamp < 12):
     print("good morning")
-elif(timestamp >= 12 and timestamp < 17):
+elif(timestamp >= 12 and timestamp < 18):
     print("good afternoon")
-elif(timestamp >= 17 and timestamp < 0):
+elif(timestamp >= 18 and timestamp < 24):
     print("good evening")       
