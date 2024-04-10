2d3b7d9797bf5ba7fadfda2bd5892a85a9434d72
Shailendrajain08 <shailendra.jain0894@gmail.com>
2024-04-10 14:02:51 -0400

    Taking User Inputs

---
+++ takingUserInput.py
@@ -0,0 +1,11 @@
+# In python, taking user input is possible by using input() funtion. This input function gives a return value as string/character hence we have to pass that into a variable. input function always consider as string if you want to use it for float, number or other data types you have to typeCast. 
+
+# a = input("Enter Your Name: ")
+
+# print("My Name is:", a)
+
+x = input('Enter First number: ')
+y = input('Enter Second number: ')
+
+print (x + y)                          #it will concatinate
+print (int(x) + int(y))
