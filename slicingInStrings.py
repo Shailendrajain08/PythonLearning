ce9ae08fb701d1b75f6d1f1fc73b82db87371be2
Shailendrajain08 <shailendra.jain0894@gmail.com>
2024-04-10 14:39:41 -0400

    string slicing

---
+++ slicingInStrings.py
@@ -0,0 +1,11 @@
+names = "Shailendra,Shaan,Shailu"
+
+print(names[0:10])
+print(names[:10])  #can remove 0
+
+# finding length 
+
+fruit = "mango"
+print(len(fruit))
+print(fruit[0:-3])
+print(fruit[-3:-1])
