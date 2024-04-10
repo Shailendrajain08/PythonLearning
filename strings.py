0b517a7257f862749f70ccdb0ee91aa738cc9dfa
Shailendrajain08 <shailendra.jain0894@gmail.com>
2024-04-10 14:17:33 -0400

    strings

---
+++ strings.py
@@ -0,0 +1,15 @@
+# Every this that is inclosed with "" or '' is known as string in python
+
+name = 'shailendra'
+friend = "shaan"
+apple = "said"
+eat = "I want to eat an apple"
+
+# print("hey", name, friend, apple, eat)
+
+# in python strings are like array of character 
+
+print(friend[4])
+print(eat[18])
+print(apple[0])
+print(name[7])
