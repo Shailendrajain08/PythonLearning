7db136ca5f1e76aacf96ba02c67a11191e9d4f8b
Shailendrajain08 <shailendra.jain0894@gmail.com>
2024-04-10 14:58:29 -0400

    String Methods

---
+++ stringsMethods.py
@@ -0,0 +1,13 @@
+# Strings are immutable
+
+a = "Shailu !!!!!!!!!!!!!!!"
+print(len(a))
+print(a)
+print(a.upper())
+print(a.lower())
+print(a.rstrip('!'))
+print(a.replace("Shailu", "Shaan"))
+print(a.split(" "))
+
+someHeading = "hello i am learnig python"
+print(someHeading.capitalize())
